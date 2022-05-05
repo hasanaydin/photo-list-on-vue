e0424cc6f257854beadd76855d2b77fa4a59060e
hasan.aydin <gecegez@gmail.com>
2022-05-05 12:20:57 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PhotoList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PhotoList from "./components/PhotoList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PhotoList,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/PhotoDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ image.url }}</h1>
+  asdasdas
+</template>
+
+<script>
+export default {
+  props: {
+    image: Object,
+  },
+};
+</script>
+<style scoped>
+@import "../assets/styles/css.scss";
+</style>
